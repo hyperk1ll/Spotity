81d767201d44ce401dbed22c3f37b5be32238518
Hilmy Tsany <hilmy.tsani@gmail.com>
2022-11-03 22:25:40 +0700

    done project

---
+++ app/src/main/java/com/example/spotity/MusicMediaPlayer.java
@@ -0,0 +1,16 @@
+package com.example.spotity;
+
+import android.media.MediaPlayer;
+
+public class MusicMediaPlayer {
+    static MediaPlayer instance;
+
+    public static MediaPlayer getInstance(){
+        if(instance == null){
+            instance = new MediaPlayer();
+        }
+        return instance;
+    }
+
+    public static int currentIndex = -1;
+}
